428c351141fea65721bf4948bf2a4188ad29f279
Taras <luchakt2@gmail.com>
2026-06-30 10:15:18 +0300

    feat: initialize project with Vite, Tailwind CSS, Zustand and Axios

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <section id="center">
+        <h1 className="text-3xl font-bold underline text-center m-20">Hello World</h1>
+      </section>
+    </>
+  )
+}
+
+export default App
